3e9ad2feb2a124b59938c03bc0e702c042297040
hsuan-ju <mis101bird@gmail.com>
2016-10-16 18:18:51 +0800

    link.insert simple validation

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -4,7 +4,8 @@ class LinkCreate extends Component{
 
     handleSubmit(event){
         event.preventDefault(); //阻止元素默認行為
-        console.log(this.refs.input.value);
+        console.log(this.refs.link.value); //ref: react的特殊屬性，取得render return的HTML DOM實體
+        Meteor.call('links.insert',this.refs.link.value); // 用call使用Meteor.method
     }
 
     render(){
@@ -12,7 +13,7 @@ class LinkCreate extends Component{
         <form onSubmit={this.handleSubmit.bind(this) /*bind the Context*/}>
             <div className="form-group">
                 <label>Link to Shorten</label>
-                    <input ref="input" className="form-control"/>
+                    <input ref="link" className="form-control"/>
             </div>
             <button className="btn btn-primary">Shorten!!</button>
         </form>
--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,3 +1,19 @@
 import {Mongo} from 'meteor/mongo';
+import validUrl from 'valid-url';
+import { check, Match } from 'meteor/check';
+// Default不會自動載入
+// 要在client和server的main.js import
+
+Meteor.methods({
+    'links.insert':function(url){
+        // validUrl.isUri(url); true: return url, false: return undefine
+        // check(value, pattern)
+        /* Match.Where(condition): 
+        如果condition為true，check不做任何事
+        如果condition為false，check throw Error 
+        */
+        check(url, Match.Where(url => validUrl.isUri(url)));
+    }
+});
 
 export const Links=new Mongo.Collection("links");
